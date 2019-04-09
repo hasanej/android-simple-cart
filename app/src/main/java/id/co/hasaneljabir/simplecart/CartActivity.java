18a446b7fc9930bebc0e056137bef28676c82f0e
Hasan El Jabir <eljabirhasan@gmail.com>
2019-04-10 06:00:00 +0700

    Add cart feature

--- app/src/main/java/id/co/hasaneljabir/simplecart/CartActivity.java
+++ app/src/main/java/id/co/hasaneljabir/simplecart/CartActivity.java
@@ -3,11 +3,11 @@ package id.co.hasaneljabir.simplecart;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class CartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_cart);
     }
 }
